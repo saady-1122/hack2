de91718f3625246af5732497c8b092ace630468a
saady-1122 <saadkhan1162003@gmail.com>
2023-10-19 02:47:09 -0700

    half program done

--- airdistance.c
+++ airdistance.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-#define 
+#define #include <stdio.h>
+#include <Math.h>
+double EARTH-RADIUS 6371.0
+double degreeToRadius(double degree){
+return degree *M-PL/180.0
+}
+double calculateDistance(double Lat1,double lon1,double lat2,double lon2){
+lat1 = degreeToRadius(lat1);
+lon1 = degreeToRadius(lon1);
+lat2 = degredToRadius(lon1);
+lon2 = degreeToRadius(lon2)
+double dlat = lat2-lat1;
+double dlon = lon2-lon1;
+
+}
